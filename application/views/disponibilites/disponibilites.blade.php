b4b76505b7c02dca78a9936f87be3d3281a22a56
Unknown <g.suppin@gmx.fr>
2019-01-06 23:18:45 +0100

    090104 5

--- application/views/disponibilites/disponibilites.blade.php
+++ application/views/disponibilites/disponibilites.blade.php
@@ -9,8 +9,10 @@
 		<p>
 			Je suis souple en terme d'organisation. Je m'adapte à toutes les situations. Je suis disponible pour des remplacements à la journée ( 1 ou plusieurs jours ), à la semaine, toute durée. J'étudie toutes les propositions de remplacement. Je suis très réactif pour répondre à vos demandes et questions. 
 		</p>
+		<input type="text" class="form-control datepicker" value="14/09/1989">
+
 		
 	</div>
 	
 </div>
-	@endsection
+@endsection
--- application/views/template/template.blade.php
+++ application/views/template/template.blade.php
@@ -7,6 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="{{ base_url() }}dist/css/app.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
 	<title>Pharmacien remplaçant | Jérôme SUPPIN</title>
@@ -64,10 +65,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<p> <h6> © Jérôme SUPPIN 2019</h6></p>
 </footer>
 
-
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js" integrity="sha256-ncetQ5WcFxZU3YIwggfwOwmewLVX4SHLBtDYnrsxooY=" crossorigin="anonymous"></script>
 </body>
-
+<script>
+	(function($){
+	$.fn.datepicker.dates['fr'] = {
+	days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],
+	daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],
+	daysMin: ["d", "l", "ma", "me", "j", "v", "s"],
+	months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
+	monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
+	today: "Aujourd'hui",
+	monthsTitle: "Mois",
+	clear: "Effacer",
+	weekStart: 1,
+	format: "dd/mm/yyyy"
+	};
+	}(jQuery));
+	
+	$('.datepicker').datepicker({
+	language: 'fr',
+	autoclose: true,
+	todayHighlight: true
+	})
+	</script>
 </html>
